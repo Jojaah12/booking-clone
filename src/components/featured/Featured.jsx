43b9d2d00e36c056346ac8e35beef8804cc9d396
Jojaah12 <mohammed.aljojah@gmail.com>
2022-07-17 18:13:31 +0200

    adding property list

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,44 @@
+import "./featured.css";
+
+const Featured = () => {
+  return (
+    <div className="featured">
+      <div className="featuredItem">
+        <img
+          src="https://r-xx.bstatic.com/xdata/images/xphoto/max1200/150492142.jpg?k=efebfcb5e00a46bcbc565288adfa7b312f3499bf211fce21abb1ab217cdd1fc2&o="
+          alt=""
+          className="featuredImg"
+        />
+        <div className="featuredTitles">
+          <h1>Dublin</h1>
+          <h2>123 properties</h2>
+        </div>
+      </div>
+
+      <div className="featuredItem">
+        <img
+          src="https://q-xx.bstatic.com/xdata/images/xphoto/max1200/147544954.jpg?k=26646a97b097b746ad0608dfe9c7c1fc6015271a14701a34aec649a9ba86cf85&o="
+          alt=""
+          className="featuredImg"
+        />
+        <div className="featuredTitles">
+          <h1>Reno</h1>
+          <h2>533 properties</h2>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img
+          src="https://r-xx.bstatic.com/xdata/images/xphoto/max1200/144102684.jpg?k=1b43e1ca23c2ee0ac8b24e734164e3bed96ad43cad6377ac03d01be95b9da5e2&o="
+          alt=""
+          className="featuredImg"
+        />
+        <div className="featuredTitles">
+          <h1>Austin</h1>
+          <h2>532 properties</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,19 @@
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
+import Featured from "../../components/featured/Featured";
 import "./home.css";
+import PropertyList from "../../components/propertyList/PropertyList";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Utforska Mer</h1>
+        <PropertyList />
+      </div>
     </div>
   );
 };
