492dc78d08982149b9607a5cb105d76420d4541d
Jojaah12 <david.aljojah@gmail.com>
2023-04-13 19:13:06 +0200

    added a link tag to navigate to login page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
-import "./navbar.css";
+import { Link } from 'react-router-dom';
+import './navbar.css';
 
 const Navbar = () => {
   return (
@@ -7,8 +8,12 @@ const Navbar = () => {
         <span className="logo">Booking.com</span>
         <div className="navItems">
           {/* <button className="navButton-blue">Registrera ditt boende</button> */}
-          <button className="navButton">Skapa konto</button>
-          <button className="navButton">Logga in</button>
+          <button className="navButton">
+            <Link to="/login">Skapa konto</Link>
+          </button>
+          <Link className="button button-link" to="/login">
+            Logga in
+          </Link>
         </div>
       </div>
     </div>
